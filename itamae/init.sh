43717b467fd9a0d2a61e19ffe4690c31df33826b
Sorah Fukumori <sorah@cookpad.com>
2019-02-23 23:54:26 +0900

    symlink site

--- itamae/init.sh
+++ itamae/init.sh
@@ -5,6 +5,7 @@ if [ ! -e ../cnw/itamae ]; then
 fi
 ln -s ../cnw/itamae cnw
 ln -s ../cnw/itamae/site.rb site.rb
+ln -s ../cnw/itamae/site site
 cp ../cnw/itamae/vars.rb vars.rb
 cp ../cnw/itamae/hocho.yml hocho.yml
 cp ../cnw/itamae/Gemfile Gemfile
