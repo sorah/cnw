358c49b752c1f261d5b9056577386712173306b8
Sorah Fukumori <sorah@cookpad.com>
2019-02-23 23:25:29 +0900

    use entry_ec2.rb for hocho-ec2 hosts

--- itamae/site/entry_ec2.rb
+++ itamae/site/entry_ec2.rb
@@ -1,5 +1,6 @@
-node[:roles] = (node.dig(:hocho_ec2, :tags, :Role) || 'base').split(',')
+node[:desired_hostname] = node.dig(:hocho_ec2, :tags, :Name)
 
+node[:roles] = (node.dig(:hocho_ec2, :tags, :Role) || 'base').split(',')
 node[:roles].each do |_|
   include_role _
 end
